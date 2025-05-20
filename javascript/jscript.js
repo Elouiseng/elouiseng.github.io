b83cc0b791c893d48cb33d2f6b030f5c8516a681
Louise <122558521+Elouiseng@users.noreply.github.com>
2025-05-20 09:17:55 +0000

    Changed from Notepad++ to VS Code; also header and footer template in js; and started with project.html 	___〆(・∀・)

--- javascript/jscript.js
+++ javascript/jscript.js
@@ -53,3 +53,44 @@ function handleSubmit(event) {
   }
 }
 
+class MyHeader extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.innerHTML = `
+			<header id="header" class="header" >
+				<div class="header-title">
+					<h3>Ela Louise Glatzeder</h3>
+					<h6>Game & Software Developer</h6>
+				</div>
+				<div class="header-nav">
+					<a id="about-me-nav-link" href="../index.html"> About Me</a>
+					<a id="projects-nav-link" href="../pages/projects.html">Projects</a>
+					<a id="resume-nav-link" href="../documents/elaglatzeder_cv_may2025_eng.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+				</div>
+
+			</header>
+			<hr class="header-main-hr">
+		`;
+	}
+}
+
+class MyFooter extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.innerHTML = `
+			<hr class="main-footer-hr">
+			<footer class="footer">
+				<p>&#169 Elouiseng 2025</p>
+			</footer>
+		`;
+	}
+}
+
+customElements.define('my-header', MyHeader);
+customElements.define('my-footer', MyFooter);
