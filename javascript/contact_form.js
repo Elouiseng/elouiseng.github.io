bd9d7344690a892d66a776fd7aed8459168bc4c8
Louise <122558521+Elouiseng@users.noreply.github.com>
2025-07-29 09:56:57 +0000

    Well; I tried; changed Button getting; ( ◡̀_◡́)ᕤ

--- javascript/contact_form.js
+++ javascript/contact_form.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!form) return;
 
-  const button = form.querySelector('button[type="submit"]'); // ✅ define this!
+  const button = document.getElementById('cfbutton');
 
   form.addEventListener('submit', async (e) => {
     console.log('submit event fired');
